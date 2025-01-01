2288409e69076ef9b8e43ea8a92ca104de124c2e
mathewsQ369 <mathewsq369@gmail.com>
2025-01-01 12:15:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+recoding the bank account management system
+"""
+"""
+After starting the system
+1. User wants to login
+2. User wants to create account
+3.Exit 
+"""
+
+
+"""
+for 1
+    ask for accountnumber and password
+    if password forgotten request for password hint
+    failure to remember after the password hint, user should contact customer service
+    
+for 2
+    ask for user information: name, emailaddr
+    user creates password and recovery password hint
+    account number generated and displayed to the user
+    user is redirected to the login page
+"""
+
+
+def mainMenu():
+    print(13 * "=")
+    print(3 * " " + "WELCOME")
+    print(13 * "=")
